db581e95c2fcc3bd98059c6bbd8dc741e098243e
matekoder <githubcomrade@atomicmail.io>
2026-05-07 14:51:50 +0100

    Update HeroSection.jsx
    Launch dApp button added

--- client/src/components/PageComponent/LP_Components/HeroSection.jsx
+++ client/src/components/PageComponent/LP_Components/HeroSection.jsx
@@ -35,35 +35,97 @@ const HeroSection = () => {
             
               <div className='wideContainer wd2'>
               
-            <p data-aos="fade-up" data-aos-easing="linear"  data-aos-duration="300" data-aos-delay="300"> Panther is building a modular solution for financial service providers to operate private trading Zones with customizable rules, defining the future of confidential on-chain finance.</p>
-            <a href={ROUTES.DeFi_APP} target="_blank" rel="nofollow noopener noreferrer">
-              <div className='glow-effects' data-aos="fade-up" data-aos-offset="-200" data-aos-easing="linear" data-aos-duration="300" data-aos-delay="600" >
-              <Link to="/contact-us" rel="noopener">
-              <button className='button'>
-                    <svg className='btn-svg' width="226" height="61" viewBox="0 0 226 61" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <g filter="url(#filter0_b_9_2218)">
-                        <rect x="0.12793" y="0.100098" width="223.5" height="61" rx="15" fill="#D9D9D9" fillOpacity="0.05"/>
-                        <rect x="1.13213" y="1.1043" width="224" height="59" rx="14" stroke="url(#paint0_linear_9_2218)" strokeWidth="2.0084"/>
-                        </g>
-                        <defs>
-                        <filter id="filter0_b_9_2218" x="-99.8721" y="-99.8999" width="542" height="273" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
-                        <feFlood floodOpacity="0" result="BackgroundImageFix"/>
-                        <feGaussianBlur in="BackgroundImageFix" stdDeviation="50"/>
-                        <feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur_9_2218"/>
-                        <feBlend mode="normal" in="SourceGraphic" in2="effect1_backgroundBlur_9_2218" result="shape"/>
-                        </filter>
-                        <linearGradient id="paint0_linear_9_2218" x1="27.0593" y1="20.2688" x2="318.3" y2="32.3193" gradientUnits="userSpaceOnUse">
-                        <stop stopColor="#87F9C2"/>
-                        <stop offset="1" stopColor="#6BB1D9"/>
-                        </linearGradient>
-                        </defs>
-                    </svg>
-                    <span>Contact Us <img width="10px" height="14px" src={arrowRight} alt='Panther Protocol' /></span>
-                </button>
-                </Link>
-              </div>
-              </a>
-          </div>
+       <p
+  data-aos="fade-up"
+  data-aos-easing="linear"
+  data-aos-duration="300"
+  data-aos-delay="300"
+>
+  Panther is building a modular solution for financial service providers to operate private trading Zones with customizable rules, defining the future of confidential on-chain finance.
+</p>
+
+<a
+  href="https://pantherdao.app"
+  target="_blank"
+  rel="nofollow noopener noreferrer"
+>
+  <div
+    className="glow-effects"
+    data-aos="fade-up"
+    data-aos-offset="-200"
+    data-aos-easing="linear"
+    data-aos-duration="300"
+    data-aos-delay="600"
+  >
+    <button className="button" type="button">
+      <svg
+        className="btn-svg"
+        width="226"
+        height="61"
+        viewBox="0 0 226 61"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g filter="url(#filter0_b_9_2218)">
+          <rect
+            x="0.12793"
+            y="0.100098"
+            width="223.5"
+            height="61"
+            rx="15"
+            fill="#D9D9D9"
+            fillOpacity="0.05"
+          />
+          <rect
+            x="1.13213"
+            y="1.1043"
+            width="224"
+            height="59"
+            rx="14"
+            stroke="url(#paint0_linear_9_2218)"
+            strokeWidth="2.0084"
+          />
+        </g>
+        <defs>
+          <filter
+            id="filter0_b_9_2218"
+            x="-99.8721"
+            y="-99.8999"
+            width="542"
+            height="273"
+            filterUnits="userSpaceOnUse"
+            colorInterpolationFilters="sRGB"
+          >
+            <feFlood floodOpacity="0" result="BackgroundImageFix" />
+            <feGaussianBlur in="BackgroundImageFix" stdDeviation="50" />
+            <feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur_9_2218" />
+            <feBlend mode="normal" in="SourceGraphic" in2="effect1_backgroundBlur_9_2218" result="shape" />
+          </filter>
+          <linearGradient
+            id="paint0_linear_9_2218"
+            x1="27.0593"
+            y1="20.2688"
+            x2="318.3"
+            y2="32.3193"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stopColor="#87F9C2" />
+            <stop offset="1" stopColor="#6BB1D9" />
+          </linearGradient>
+        </defs>
+      </svg>
+      <span>
+        Launch dApp{" "}
+        <img
+          width="10"
+          height="14"
+          src={arrowRight}
+          alt="Arrow right"
+        />
+      </span>
+    </button>
+  </div>
+</a>
       </HeroWrapper>
      
      <LogoRolls />
